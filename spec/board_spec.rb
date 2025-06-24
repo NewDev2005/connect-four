94df3636d5b401ca5a8f411b8c31b463cc1d8e7b
krypto <clayjeansen3@gmail.com>
2025-06-24 21:56:41 +0530

    written test case for the set_the_board method of board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/board"
+require_relative "spec_helper"
+
+describe Board do
+  subject(:board) { described_class.new }
+
+  describe "#set_the_board" do
+    it "checks all the elements of the @array has same value" do
+      board.set_the_board
+      arr = board.array
+      hollow_circle = "\u25EF"
+      expect(arr).to all(eq(hollow_circle))
+    end
+  end
+end
