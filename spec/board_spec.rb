2a3b6239c0309c03aecbb47716b489d26f688bdd
krypto <clayjeansen3@gmail.com>
2025-07-25 23:59:40 +0530

    add test cases for  #check_invalid_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,3 +14,61 @@ describe Board do
     end
   end
 end
+
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe "#displays_the_board_before_the_game_begins" do
+
+    it "displays the board with only hollow circles once before the game begins" do
+      expect(game).to receive(:displays_the_board_before_the_game_begins).once
+      game.displays_the_board_before_the_game_begins
+    end
+  end
+
+  describe "#check_invalid_move" do
+
+    context "when the user input is  a valid move" do
+      before do
+        valid_move = "0,1"
+        allow(game).to receive(:gets).and_return(valid_move)
+      end
+
+      it "stops prompting for the user input and does not display the invalid error message" do
+        valid_move = "0,1"
+        error_message = "INVALID INPUT!>> re-enter a valid coord".colorize(:red)
+        expect(game).not_to receive(:puts).with(error_message)
+        game.check_invalid_move(valid_move)
+      end
+    end
+
+    context "when the user enters exit" do
+      before do
+        user_input = "exit"
+        allow(game).to receive(:gets).and_return(user_input)
+      end
+
+      it "stops the loop and does not display the error message" do
+        user_input = "exit"
+        error_message = "INVALID INPUT!>> re-enter a valid coord".colorize(:red)
+        expect(game).not_to receive(:puts).with(error_message)
+        game.check_invalid_move(user_input)
+      end
+    end
+
+    context "when the user enters an invalid move once and then a valid move" do
+      before do
+        invalid_move = "15"
+        allow(game).to receive(:gets).and_return(invalid_move)
+      end
+
+      it "keeps prompting for the user input  and displays the error message" do
+        invalid_move = "15"
+        error_message = "INVALID INPUT!>> re-enter a valid coord".colorize(:red)
+        expect(game).to receive(:puts).with(error_message)
+        game.check_invalid_move(invalid_move)
+        # game.check_invalid_move(valid_move)
+      end
+    end
+  end
+end
