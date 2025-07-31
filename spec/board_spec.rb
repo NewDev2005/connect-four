2faa27df6834d0c4bd5e04af4cb74b56fd0871b1
krypto <clayjeansen3@gmail.com>
2025-08-01 00:27:06 +0530

    test cases added for #illegal_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 # require_relative "../lib/board"
 require_relative "../lib/game"
+require_relative "../lib/module"
 require_relative "spec_helper"
 
 describe Board do
@@ -141,6 +142,105 @@ describe Game do # rubocop:disable Metrics/BlockLength
       it "returns true" do
         expect(game.check_winner?("3,3", :red)).to eql(true)
       end
+    end
+
+    context "when three chips of the same color and one chip of different color are placed vertically" do
+      before do
+        game.check_winner?("1,0", :red)
+        game.check_winner?("2,0", :green)
+        game.check_winner?("3,0", :red)
+      end
+      it "returns false" do
+        expect(game.check_winner?("4,0", :red)).to eql(false)
+      end
+    end
+
+    context "when two chips of the red color and two chip of green color are placed horizontally" do
+      before do
+        game.check_winner?("0,0", :red)
+        game.check_winner?("0,1", :green)
+        game.check_winner?("0,2", :red)
+      end
+      it "returns false" do
+        expect(game.check_winner?("0,3", :green)).to eql(false)
+      end
+    end
+
+    context "when two chips of the red color and two chip of green color are placed diagonally" do
+      before do
+        game.check_winner?("3,0", :red)
+        game.check_winner?("2,1", :green)
+        game.check_winner?("1,2", :red)
+      end
+      it "returns false" do
+        expect(game.check_winner?("0,3", :green)).to eql(false)
+      end
+    end
+  end
+
+  describe "#illegal_move" do
+    context "when the player place their chip above another chip" do
+      before do
+        board = game.instance_variable_get(:@board)
+        board.set_the_board
+        game.register_move("0,2", :red)
+      end
+      it " doesn't displays the error message" do
+        error_message = "Sorry but This not how this game works!".colorize(:red)
+        expect(game).not_to receive(:puts).with(error_message)
+        game.illegal_move("1,2")
+      end
+    end
+
+    context "when the player doesn't place thier chip above another chip" do
+      before do
+        board = game.instance_variable_get(:@board)
+        board.set_the_board
+      end
+      it "displays the error message" do
+        coord = "3,4"
+        error_message = "Sorry but This not how this game works!".colorize(:red)
+        expect(game).to receive(:puts).with(error_message)
+        game.illegal_move(coord)
+      end
+    end
+  end
+
+  describe "#illegal_move2" do # rubocop:disable Metrics/BlockLength
+    context "when the player tries to place their chip at a coordinate where already a chip has been placed" do
+      before do
+        board = game.instance_variable_get(:@board)
+        board.set_the_board
+        game.register_move("0,0", :red)
+      end
+      it "displays the error message" do
+        coord = "0,0"
+        error_message = "A chip has been placed at #{coord} already kindly try a different move!".colorize(:yellow)
+        expect(game).to receive(:puts).with(error_message).once
+        game.illegal_move2(coord)
+      end
+    end
+
+    context "when the player doesn't place their move at a coordinate where already a chip has been placed" do
+      before do
+        board = game.instance_variable_get(:@board)
+        board.set_the_board
+        game.register_move("0,0", :red)
+      end
+      it "doesn't display the error message" do
+        coord = "1,0"
+        error_message = "A chip has been placed at #{coord} already kindly try a different move!".colorize(:yellow)
+        expect(game).not_to receive(:puts).with(error_message)
+        game.illegal_move2(coord)
+      end
+    end
+
+    it "returns the player input" do
+      board = game.instance_variable_get(:@board)
+      board.set_the_board
+      game.register_move("0,1", :red)
+      player_input = "1,0"
+      expect(game.illegal_move2(player_input)).to eql(player_input)
     end
   end
 end
